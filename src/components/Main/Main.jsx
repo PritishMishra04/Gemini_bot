24e007e0f734df490d28908824c5d721296a8012
Pritish Mishra <mishrapritish661@gmail.com>
2024-08-16 23:15:55 +0530

    final commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,79 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import ContextProvider, { Context } from '../../context/Contaxt'
+const Main = () => {
+    const {onSent,recentPrompt,showResult,loading,resultData,setInput,input}=useContext(Context)
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+
+      {!showResult
+      ?<>
+        <div className="greet">
+            <p><span>Hello,dev</span></p>
+            <p>How can I help you today</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggect a Beautiful city for a road trip</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Breaf on an urban city architecture</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Tasks to inprove team building skills</p>
+                <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Simplify the code to make it more readable</p>
+                <img src={assets.code_icon} alt="" />
+            </div>
+        </div>
+      </>
+      :<div className='result'>
+        <div className="result-title">
+          <img src={assets.user_icon} alt="" />
+          <p>{recentPrompt}</p>
+        </div>
+        <div className="result-data">
+          <img src={assets.gemini_icon} alt="" />
+          {loading
+          ?
+          <div className="loader">
+            <hr />
+            <hr />
+            <hr />
+          </div>       
+          :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
+          }
+        </div>
+        </div>
+      }
+
+        
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt' />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit tempora, rerum molestiae atque non veritatis ipsum! Quia ea at, eos nulla voluptas possimus rerum, quod porro nobis necessitatibus soluta inventore!
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/context/Contaxt.jsx
@@ -0,0 +1,69 @@
+import { createContext,useState } from "react";
+import run from "../config/gemini";
+
+export const Context= createContext();
+
+const ContextProvider=(props) => {
+
+    const [input,setInput]=useState(" ");
+    const [recentPrompt,setRecentPrompt]=useState(" ");
+    const [prevPrompts,setPrevPrompts]=useState([]);
+    const [showResult,setShowResult]=useState(false);
+    const [loading,setLoading]=useState(false);
+    const [resultData,setResultData]=useState(" ");
+
+    // const delayPara=(index,nextWord)=>{
+    //     setTimeout(function(){
+    //         setResultData(prev=>prev+nextWord);
+    //     },75*index)
+    // }
+
+    const onSent = async(prompt) => {
+        setResultData("")
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        setPrevPrompts(prev=>[...prev,input])
+        const response=await run(input)
+        let responseArray=response.split("**");
+        let newArray;
+        for(let i=0; i<responseArray.length;i++){
+            if(i===0 || i%2 !== 1){
+                newArray+=responseArray[i];
+            }
+            else{
+                newArray+="<b>"+responseArray[i]+"</b>";
+            }
+        }
+        let newResponse=newArray.split("*").join("</br>");
+        let newResponseArray = newResponse.split(" ");
+        // for(let i=0;i<newResponse.length;i++){
+        //     const nextWord = newResponseArray[i];
+        //     delayPara(i,nextWord+" ");
+        // }
+        setResultData(newResponse)
+        setLoading(false)
+        setInput("")
+    }
+
+    const contextValue={
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
+    }
+
+    return(
+        <Context.Provider value={contextValue}>
+        {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider
